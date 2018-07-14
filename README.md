61e882e813e24e654459f6d22e7d70c5b6df70a5
Marc Hertzog <marc@marcarea.com>
2018-07-14 19:49:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# HTML elements
+
+> How various CSS frameworks style HTML elements.
+
+This repo contains an HTML page with all HTML5 elements for styling. I added a style switcher ([like it's 2001](https://alistapart.com/article/alternate)) to dynamically switch between the style sheets of various CSS frameworks and studying how they deal with HTML elements.
+
+## HTML source
+
+- [WHATWG](https://html.spec.whatwg.org/dev/semantics.html)
+- [W3C](http://w3c.github.io/html/semantics.html)
+- [MDN](https://developer.mozilla.org/docs/Web/HTML/Element)
+
+## Inspiration
+
+- [*HTML page with (most) all elements, for styling*](https://stackoverflow.com/questions/4790046/html-page-with-most-all-elements-for-styling)
+- [HTML Elements Index](https://meiert.com/en/indices/html-elements/)
+- [Native form elements](http://nativeformelements.com)
+
+## Credits
+
+- [Strawberry pic by Toni Cuenca](https://www.pexels.com/photo/berry-blur-close-up-delicious-566888/)
+- [Image map by WHATWG](https://html.spec.whatwg.org/multipage/image-maps.html#image-maps)
+- [PDF file by Adobe](https://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/pdf_open_parameters.pdf)
+- [Video by Leonard Lee](https://www.youtube.com/watch?v=Z_4TlZCvYL4)
+- [Audio by Daniel Simion](http://soundbible.com/2210-SOS-Morse-Code.html)
+- [Icons: Feather](https://feathericons.com)
+
+## TODO
+
+- `MathML`
+- `SVG`
+- `Canvas`
---
+++ static/css-switcher.js
@@ -0,0 +1,190 @@
+// https://github.com/troxler/awesome-css-frameworks
+// https://cdnjs.com/
+// https://rawgit.com
+// https://unpkg.com/
+
+let styles = {
+    'Reset and Normalize': [
+        {
+            name: 'micro-css-reset-1.0.0',
+            href: 'https://unpkg.com/css-micro-reset@1.0.0/micro-css-reset.css',
+            source: 'https://github.com/vladocar/CSS-Micro-Reset',
+        },
+        {
+            name: 'minireset-0.0.2',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/minireset.css/0.0.2/minireset.min.css',
+            source: 'https://github.com/jgthms/minireset.css',
+        },
+        {
+            name: 'normalize-8.0.0',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
+            source: 'https://github.com/necolas/normalize.css',
+        },
+        {
+            name: 'normalize-opentype-0.2.4',
+            href: 'https://cdn.rawgit.com/kennethormandy/normalize-opentype.css/v0.2.4/normalize-opentype.css',
+            source: 'https://github.com/kennethormandy/normalize-opentype.css',
+        },
+        {
+            name: 'sanitize-2.0.0',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/sanitize.css/2.0.0/sanitize.min.css',
+            source: 'https://github.com/csstools/sanitize.css',
+        },
+    ],
+    'Lightweight': [
+        {
+            name: 'chota-0.4.2',
+            href: 'https://unpkg.com/chota@0.4.2/dist/chota.min.css',
+            source: 'https://github.com/jenil/chota',
+        },
+        {
+            name: 'milligram-1.3.0',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css',
+            source: 'https://github.com/milligram/milligram',
+        },
+        {
+            name: 'mini-3.0.0',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.0/mini-default.min.css',
+            source: 'https://github.com/Chalarangelo/mini.css',
+        },
+        {
+            name: 'mobi-3.1.1',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/mobi.css/3.1.1/mobi.min.css',
+            source: 'https://github.com/mobi-css/mobi.css',
+        },
+        {
+            name: 'pure-1.0.0',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/pure/1.0.0/pure-min.css',
+            source: 'https://github.com/pure-css/pure/',
+        },
+        {
+            name: 'sakura-1.0.0',
+            href: 'https://cdn.rawgit.com/oxalorg/sakura/1.0.0/css/sakura.css',
+            source: 'https://github.com/oxalorg/sakura',
+        },
+        {
+            name: 'wing-0.1.9',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/wingcss/0.1.9/wing.min.css',
+            source: 'https://github.com/kbrsh/wing',
+        },
+    ],
+    'General': [
+        {
+            name: 'blaze-3.6.3',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/blaze/3.6.3/blaze.min.css',
+            source: 'https://github.com/BlazeCSS/blaze',
+        },
+        {
+            name: 'bootstrap-3.3.7',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css',
+            source: 'https://github.com/twbs/bootstrap',
+        },
+        {
+            name: 'bootstrap-4.1.2',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css',
+            source: 'https://github.com/twbs/bootstrap',
+        },
+        {
+            name: 'base-4.0.10',
+            href: 'https://cdn.rawgit.com/getbase/base/v4.0.10/css/index.css',
+            source: 'https://github.com/getbase/base',
+        },
+        {
+            name: 'bulma-0.7.1',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css',
+            source: 'https://github.com/jgthms/bulma',
+        },
+        {
+            name: 'kube-6.5.2',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/kube/6.5.2/css/kube.min.css',
+            source: 'https://github.com/imperavi/kube',
+        },
+        {
+            name: 'primer-10.7.0',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/Primer/10.7.0/build.css',
+            source: 'https://github.com/primer/primer-css',
+        },
+        {
+            name: 'spectre-0.5.3',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/spectre.css/0.5.3/spectre.min.css',
+            source: 'https://github.com/picturepan2/spectre',
+        },
+        {
+            name: 'tachyons-4.9.1',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/tachyons/4.9.1/tachyons.min.css',
+            source: 'https://github.com/tachyons-css/tachyons/',
+        },
+        {
+            name: 'uikit-2.27.4',
+            href: 'https://unpkg.com/uikit@v2.27.4/dist/css/uikit.css',
+            source: 'https://github.com/uikit/uikit',
+        },
+    ],
+    'Work in progress': [
+        {
+            name: 'base-elements',
+            href: 'css/base-elements.css',
+            source: '',
+        },
+    ],
+}
+
+let setActiveStyleSheet = function (title) {
+    [].forEach.call(
+        document.querySelectorAll('link'),
+        function (el) {
+          if (el.getAttribute('rel').indexOf('style') != -1 && el.getAttribute('title')) {
+            el.disabled = true
+            if (el.getAttribute('title') == title) {
+              el.disabled = false
+            }
+          }
+        }
+    )
+}
+
+let toolbar = document.createElement('div')
+toolbar.id = 'toolbar'
+toolbar.innerHTML = `
+  <label for="switch-css">Switch CSS</label>
+  <select id="switch-css">
+      <option value="">User Agent Style Sheet</option>
+  </select>`
+document.querySelector('body').prepend(toolbar)
+
+let switchElement = document.querySelector('#switch-css')
+
+Object.keys(styles).forEach(function (key) {
+
+    // Create <optgroup>.
+    let optgroup = document.createElement('optgroup')
+    optgroup.label = key
+
+    styles[key].forEach(function (item) {
+
+        // Add <link> elements in the <head> section.
+        let link = document.createElement('link')
+        link.rel = 'alternate stylesheet'
+        link.href = item.href
+        link.title = item.name
+        document.head.appendChild(link)
+
+        // Add <option>s to <optgroup>.
+        let option = document.createElement('option')
+        option.value = item.name
+        option.text = item.name.replace('-', ' ')
+        optgroup.appendChild(option)
+
+    })
+
+    // Add <optgroup> to <select>.
+    switchElement.appendChild(optgroup)
+
+})
+
+switchElement.addEventListener('change', function (e) {
+    setActiveStyleSheet(e.target.value)
+})
+
+let body = document.querySelector('body')
+body.style.paddingTop = document.querySelector('#toolbar').offsetHeight + 'px'
