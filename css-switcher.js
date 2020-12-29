e77ca65549ca63bf677b851a5b4f7f2925c87603
kemar <marc@marcarea.com>
2020-12-29 19:33:55 +0100

    Unpin versions

--- css-switcher.js
+++ css-switcher.js
@@ -5,43 +5,28 @@
 
 let styles = [
     {
-        "name": "awsm-2.1.0",
-        "href": "https://unpkg.com/awsm.css@2.1.0/dist/awsm.min.css",
+        "name": "awsm",
+        "href": "https://unpkg.com/awsm.css/dist/awsm.min.css",
         "source": "https://github.com/igoradamenko/awsm.css"
     },
     {
-        "name": "base-4.0.10",
-        "href": "https://cdn.jsdelivr.net/gh/getbase/base@v4.0.10/css/index.css",
-        "source": "https://github.com/getbase/base"
-    },
-    {
-        "name": "blaze-3.6.3",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/blaze/3.6.3/blaze.min.css",
-        "source": "https://github.com/BlazeCSS/blaze"
-    },
-    {
-        "name": "bootstrap-3.3.7",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css",
+        "name": "bootstrap",
+        "href": "https://unpkg.com/bootstrap/dist/css/bootstrap.min.css",
         "source": "https://github.com/twbs/bootstrap"
     },
     {
-        "name": "bootstrap-4.1.2",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css",
-        "source": "https://github.com/twbs/bootstrap"
-    },
-    {
-        "name": "bulma-0.7.1",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css",
+        "name": "bulma",
+        "href": "https://unpkg.com/bulma",
         "source": "https://github.com/jgthms/bulma"
     },
     {
-        "name": "chota-0.4.2",
-        "href": "https://unpkg.com/chota@0.4.2/dist/chota.min.css",
+        "name": "chota",
+        "href": "https://unpkg.com/chota",
         "source": "https://github.com/jenil/chota"
     },
     {
-        "name": "hyper-reset-css-1.6.0",
-        "href": "https://unpkg.com/hyper-reset-css@1.6.0/dist/reset.min.css",
+        "name": "hyper-reset-css",
+        "href": "https://unpkg.com/hyper-reset-css",
         "source": "https://github.com/heyallan/hyper-reset-css"
     },
     {
@@ -50,43 +35,33 @@ let styles = [
         "source": "https://github.com/imperavi/kube"
     },
     {
-        "name": "lighter-1.1.1",
-        "href": "https://cdn.jsdelivr.net/gh/amine1107/lighter@V1.1.1/build/lighter.min.css",
-        "source": "https://github.com/amine1107/lighter"
-    },
-    {
-        "name": "marx-3.0.3",
-        "href": "https://unpkg.com/marx-css@3.0.3/css/marx.min.css",
+        "name": "marx",
+        "href": "https://unpkg.com/marx-css/css/marx.min.css",
         "source": "https://github.com/mblode/marx"
     },
     {
-        "name": "micro-css-reset-1.0.0",
-        "href": "https://unpkg.com/css-micro-reset@1.0.0/micro-css-reset.css",
-        "source": "https://github.com/vladocar/CSS-Micro-Reset"
-    },
-    {
-        "name": "milligram-1.3.0",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css",
+        "name": "milligram",
+        "href": "https://unpkg.com/milligram",
         "source": "https://github.com/milligram/milligram"
     },
     {
-        "name": "mini-3.0.0",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.0/mini-default.min.css",
+        "name": "mini-3.0.1",
+        "href": "https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css",
         "source": "https://github.com/Chalarangelo/mini.css"
     },
     {
-        "name": "minireset-0.0.2",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/minireset.css/0.0.2/minireset.min.css",
+        "name": "minireset",
+        "href": "https://cdn.jsdelivr.net/gh/jgthms/minireset.css@master/minireset.min.css",
         "source": "https://github.com/jgthms/minireset.css"
     },
     {
-        "name": "mobi-3.1.1",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/mobi.css/3.1.1/mobi.min.css",
+        "name": "mobi",
+        "href": "https://unpkg.com/mobi.css/dist/mobi.min.css",
         "source": "https://github.com/mobi-css/mobi.css"
     },
     {
         "name": "mvp",
-        "href": "https://cdn.jsdelivr.net/gh/andybrewer/mvp@master/mvp.css",
+        "href": "https://unpkg.com/mvp.css",
         "source": "https://github.com/andybrewer/mvp"
     },
     {
@@ -95,68 +70,68 @@ let styles = [
         "source": "https://github.com/xz/new.css"
     },
     {
-        "name": "normalize-8.0.0",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css",
+        "name": "normalize",
+        "href": "https://unpkg.com/normalize.css",
         "source": "https://github.com/necolas/normalize.css"
     },
     {
-        "name": "normalize-opentype-0.2.4",
-        "href": "https://cdn.jsdelivr.net/gh/kennethormandy/normalize-opentype.css@v0.2.4/normalize-opentype.css",
-        "source": "https://github.com/kennethormandy/normalize-opentype.css"
-    },
-    {
-        "name": "primer-10.7.0",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/Primer/10.7.0/build.css",
+        "name": "primer",
+        "href": "https://unpkg.com/@primer/css/dist/primer.css",
         "source": "https://github.com/primer/primer-css"
     },
     {
-        "name": "pure-1.0.0",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/pure/1.0.0/pure-min.css",
+        "name": "pure",
+        "href": "https://unpkg.com/purecss",
         "source": "https://github.com/pure-css/pure/"
     },
     {
-        "name": "sakura-1.0.0",
-        "href": "https://cdn.jsdelivr.net/gh/oxalorg/sakura@1.0.0/css/sakura.css",
+        "name": "sakura",
+        "href": "https://unpkg.com/sakura.css/css/sakura.css",
         "source": "https://github.com/oxalorg/sakura"
     },
     {
-        "name": "sanitize-2.0.0",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/sanitize.css/2.0.0/sanitize.min.css",
+        "name": "sanitize",
+        "href": "https://unpkg.com/sanitize.css",
         "source": "https://github.com/csstools/sanitize.css"
     },
     {
         "name": "skeleton-2.0.4",
-        "href": "https://cdn.jsdelivr.net/gh/oxalorg/sakura@1.0.0/css/sakura.css",
+        "href": "http://getskeleton.com/dist/css/skeleton.css",
         "source": "https://github.com/dhg/Skeleton"
     },
     {
-        "name": "spectre-0.5.3",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/spectre.css/0.5.3/spectre.min.css",
+        "name": "spectre",
+        "href": "https://unpkg.com/spectre.css/dist/spectre.min.css",
         "source": "https://github.com/picturepan2/spectre"
     },
     {
-        "name": "style.css-1.0.0",
-        "href": "https://unpkg.com/style.css@1.0.0",
+        "name": "style.css",
+        "href": "https://unpkg.com/style.css",
         "source": "https://github.com/css-pkg/style.css"
     },
     {
-        "name": "superstylin-1.0.3",
-        "href": "https://unpkg.com/superstylin@1.0.3",
+        "name": "stylizer",
+        "href": "https://cdn.jsdelivr.net/npm/stylizercss/build/stylizer.min.css",
+        "source": "https://github.com/amine1107/Stylizer"
+    },
+    {
+        "name": "superstylin",
+        "href": "https://unpkg.com/superstylin",
         "source": "https://github.com/caiogondim/superstylin"
     },
     {
-        "name": "tachyons-4.9.1",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/tachyons/4.9.1/tachyons.min.css",
+        "name": "tachyons",
+        "href": "https://unpkg.com/tachyons",
         "source": "https://github.com/tachyons-css/tachyons/"
     },
     {
-        "name": "tufte-1.4",
+        "name": "tufte",
         "href": "https://cdn.jsdelivr.net/gh/edwardtufte/tufte-css@gh-pages/tufte.css",
         "source": "https://github.com/edwardtufte/tufte-css"
     },
     {
-        "name": "turret-5.1.3",
-        "href": "https://unpkg.com/turretcss@5.1.3/dist/turretcss.min.css",
+        "name": "turret",
+        "href": "https://unpkg.com/turretcss/dist/turretcss.min.css",
         "source": "https://github.com/turretcss/turretcss"
     },
     {
@@ -165,13 +140,13 @@ let styles = [
         "source": "https://github.com/devinhunt/typebase.css"
     },
     {
-        "name": "uikit-2.27.4",
-        "href": "https://unpkg.com/uikit@v2.27.4/dist/css/uikit.css",
+        "name": "uikit",
+        "href": "https://unpkg.com/uikit/dist/css/uikit.css",
         "source": "https://github.com/uikit/uikit"
     },
     {
-        "name": "wing-0.1.9",
-        "href": "https://cdnjs.cloudflare.com/ajax/libs/wingcss/0.1.9/wing.min.css",
+        "name": "wing",
+        "href": "https://unpkg.com/wingcss",
         "source": "https://github.com/kbrsh/wing"
     },
     {
